902132b75c462bb56886b0f979b5d374ce93dee0
duanyuxiang <duanyx@belink.com>
2020-07-09 15:15:31 +0800

    first commit

---
+++ cmake-build-debug/CMakeFiles/PAT1026.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PAT1026.dir/main.c.obj"
+  "PAT1026.exe"
+  "PAT1026.exe.manifest"
+  "PAT1026.pdb"
+  "libPAT1026.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/PAT1026.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+//MrHaddis
+//https://github.com/MrHaddis/PAT1026
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
